354cb6ff17ee7a254e80d49e14914be7828e10d4
Gabriel Andres <gabriel.andres.my@gmail.com>
2025-05-13 16:29:51 -0400

    ADSADS

--- app.py
+++ app.py
@@ -51,9 +51,6 @@ if uploaded_files:
     if tablas_totales:
         df_final = pd.concat(tablas_totales, ignore_index=True)
         
-        # Eliminar filas que comienzan con 'Productos' o 'Estado' (a partir de la fila 2)
-        df_final = df_final[~df_final.iloc[2:, 0].str.startswith('Estado')]
-
         # Eliminar filas que comienzan con 'Productos'
         df_final = df_final[~df_final.iloc[:, 0].str.startswith('Productos')]
 
